d902479cc0d5750cafd5d591f40ae70bf710be51
Rich Bell <bellrichm@gmail.com>
2026-01-09 06:50:12 -0500

    add a uni test

---
+++ bin/user/tests/unit/test_unit_simpleclass.py
@@ -0,0 +1,28 @@
+#    Copyright (c) 2026 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=invalid-name
+
+import unittest
+
+import user.tests.helpers
+
+import user.simpleclass
+
+class TestUnitSimpleClass(unittest.TestCase):
+    def test_return_first_value(self):
+        first_value = 'foo'
+        second_value = 'bar'
+
+        SUT = user.simpleclass.SimpleClass()
+
+        return_value = SUT.return_first_value(first_value, second_value)
+
+        self.assertEqual(return_value, first_value)
+
+if __name__ == '__main__':
+    user.tests.helpers.run_tests()
